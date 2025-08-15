dac911edc6ff2a0ca8d2d6aae92bbab0b32fe100
zeeshanaslam <zeeshanaslam7999@gmail.com>
2025-08-15 20:33:44 +0500

    hello added

--- pages/loginpage.py
+++ pages/loginpage.py
@@ -2,6 +2,7 @@ from playwright.sync_api import Page,expect
 
 class LoginPage:
     def __init__(self,page=Page):
+        #hello
         self.page=page
         self.username=page.get_by_placeholder("Username")
         self.password=page.get_by_placeholder("Password")
